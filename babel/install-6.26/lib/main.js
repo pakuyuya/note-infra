20979a83231cda009f1ed8c99fddfb10a1d4ac54
pakuyuya <pakuyuya@yahoo.co.jp>
2018-05-08 01:59:49 +0900

    add babel

---
+++ babel/install-6.26/lib/main.js
@@ -0,0 +1,24 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var x = 1;
+var c = 'a';
+
+consle.log(x);
+console.log(c);
+
+var Class = function Class() {
+  _classCallCheck(this, Class);
+
+  this.n = 1;
+};
+
+;
+
+var instance = new Class();
+console.log(instance);
+
+(function () {
+  return console.log('lambda');
+})();
